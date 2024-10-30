60c1148341fa58883f497c742e99cf9a0e7112c5
Cúán Tilson <cuantilson@gmail.com>
2024-10-30 09:06:40 +0200

    Part 5

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import pkg from 'pg';
+import dotenv from 'dotenv';
+
+const environment = process.env.NODE_ENV;
+
+dotenv.config();
 
 const port = 3001;
 const { Pool } = pkg;
@@ -54,11 +59,11 @@ app.delete('/delete/:id', (req, res) => {
 
 const openDb = () => {
     const pool = new Pool({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'todo',
-        password: '1234',
-        port: 5432
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.NODE_ENV === 'development' ? process.env.DB_NAME : process.env.TEST_DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT
     });
     return pool;
 };
