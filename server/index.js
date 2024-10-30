e1b9f753dcfa82e57d5aca23af46839a7d99b7a0
Cúán Tilson <cuantilson@gmail.com>
2024-10-30 12:27:14 +0200

    Part 5.2

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { pool } from './helper/db.js';
+import { toDoRouter } from './routers/toDoRouter.js';
 
 const port = process.env.PORT;
 
@@ -8,44 +8,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/', toDoRouter)
 
-app.get('/', (req, res) => {
-    pool.query('SELECT * FROM task', (error, result) => {
-        if (error) {
-            return res.status(500).json({ error: error.message });
-        }
-        return res.status(200).json(result.rows);
-    });
-});
-
-app.post('/create', (req, res) => {
-    pool.query(
-        'INSERT INTO task (description) VALUES ($1) RETURNING *',
-        [req.body.description],
-        (error, result) => {
-            if (error) {
-                return res.status(500).json({ error: error.message });
-            }
-            return res.status(200).json({ id: result.rows[0].id });
-        }
-    );
-});
-
-app.delete('/delete/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-
-    pool.query(
-        'DELETE FROM task WHERE id = $1',
-        [id],
-        (error, result) => {
-            if (error) {
-                return res.status(500).json({ error: error.message });
-            }
-            return res.status(200).json({ id: id });
-        }
-    );
-});
-
-
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({ error: err.message });
+})
 
 app.listen(port);
