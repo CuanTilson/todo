d4cf38d772a44b3bd244baf4b3398cf55c447754
Cúán Tilson <cuantilson@gmail.com>
2024-11-06 11:22:22 +0200

    Checks if user is already in database to avoid printing large message in console

--- server/helper/test.js
+++ server/helper/test.js
@@ -16,6 +16,12 @@ const initializeTestDb = async () => {
 // Insert a test user with a hashed password
 const insertTestUser = async (email, password) => {
     try {
+        const userCheck = await pool.query('SELECT * FROM account WHERE email = $1', [email]);
+        if (userCheck.rows.length > 0) {
+            console.log(`Test user ${email} already exists.`);
+            return;
+        }
+
         const hashedPassword = await bcrypt.hash(password, 10); // Use bcrypt to hash the password
         await pool.query('INSERT INTO account (email, password) VALUES ($1, $2)', [email, hashedPassword]);
         console.log(`Test user ${email} inserted successfully.`);
